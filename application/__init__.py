436375cfec915a4a460ece13d10e040af5f87428
SarahSoutoul <sarahsoutoul.uk@gmail.com>
2023-11-03 12:34:45 +0000

    add env variable

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wtsxwycy:EmqxAumxxNoYdhnqQFqvnDYjmaLD0QZu@trumpet.db.elephantsql.com/wtsxwycy'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
 from application import routes
